795b65800123e1f08b3b1c22057bfbd1b25f21da
manuel <manuel.casanova.ucv@gmail.com>
2026-03-24 16:59:16 -0400

    Commit 2

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,11 +1,14 @@
-import { IsNotEmpty } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 
 export class CreateTaskDto {
-   
+      
+      @IsString()
       @IsNotEmpty()
+      @MinLength(3)
       title: string;
     
+      @IsBoolean()
       @IsNotEmpty()
       done: boolean;
     
